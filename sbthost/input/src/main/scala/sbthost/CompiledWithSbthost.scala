f164eef59ee618b328e6f16396644463c637792f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-09 12:51:53 +0200

    Hijack reporter. It fails because xsbti.DelegatingReporter.

--- sbthost/input/src/main/scala/sbthost/CompiledWithSbthost.scala
+++ sbthost/input/src/main/scala/sbthost/CompiledWithSbthost.scala
@@ -1,6 +1,6 @@
 package sbthost
 
 object CompiledWithSbthost {
-
-
+  1 + 2
+  val x = List.empty[Int].head
 }
--- sbthost/nsc/src/main/scala/scala/meta/internal/sbthost/SbthostPlugin.scala
+++ sbthost/nsc/src/main/scala/scala/meta/internal/sbthost/SbthostPlugin.scala
@@ -1,45 +1,96 @@
 package scala.meta.internal.sbthost
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import scala.collection.mutable
+import scala.reflect.internal.util.Position
+import scala.reflect.internal.util.RangePosition
+import scala.reflect.internal.util.SourceFile
 import scala.tools.nsc.{Global, Phase}
 import scala.tools.nsc.plugins.{Plugin, PluginComponent}
-import scala.reflect.internal.util._
+import scala.tools.nsc.reporters.Reporter
+import scala.meta.internal.semantic.{schema => s}
+import scala.reflect.internal.util.OffsetPosition
+import scala.tools.nsc.reporters.StoreReporter
+
+case class SbthostConfig(sourceroot: Path, targetroot: Path) {
+  val metainf = targetroot.resolve("META-INF")
+  val semanticdb = metainf.resolve("semanticdb")
+  def relativePath(path: Path) = sourceroot.relativize(path)
+  def semanticdbPath(relativePath: Path) = {
+    val sibling =
+      relativePath.getFileName.toString.stripSuffix(".scala") + ".semanticdb"
+    semanticdb
+      .resolve(relativePath)
+      .resolveSibling(sibling)
+      .toAbsolutePath
+  }
+}
 
 class SbthostPlugin(val global: Global) extends Plugin {
   val name = "sbthost"
   val description = "Compiler plugin for sbt v1.0 migration."
   val components = List[PluginComponent](SbthostComponent)
+  val config = SbthostConfig(
+    sourceroot = Paths.get(sys.props("user.dir")),
+    targetroot = Paths.get(
+      global.settings.outputDirs.getSingleOutput
+        .map(_.file.toURI)
+        .getOrElse(new File(global.settings.d.value).toURI))
+  )
   private object SbthostComponent extends PluginComponent {
     val global = SbthostPlugin.this.global
     import global._
     override val runsAfter = List("typer")
     override val runsRightAfter = Some("typer")
     val phaseName = "sbthost"
-    override def newPhase(prev: Phase) = new GlobalPhase(prev) {
-      override def run() = {
-        global.reporter.info(NoPosition, s"UNIT:!!!!", true)
-        val count = global.currentRun.units.length
-        val x: Iterator[Seq[String]] = global.currentRun.units.map(_.source.file.path.split("/"))
-        var start = x.next()
-        for (next <- x) {
-          for (i <- next.indices) {
-            if (i < start.length && start.lift(i) != next.lift(i)) {
-              start = start.take(i)
+    def getMessages(source: SourceFile) =
+      global.reporter match {
+        case reporter: StoreReporter =>
+          reporter.infos.withFilter(_.pos.source == source).map { info =>
+            val range = info.pos match {
+              case p: RangePosition => s.Range(p.start, p.end)
+              case p: OffsetPosition => s.Range(p.start, -1)
+              case _ => s.Range(-1, -1)
             }
+            val severity = info.severity.id match {
+              case 0 => s.Message.Severity.INFO
+              case 1 => s.Message.Severity.WARNING
+              case 2 => s.Message.Severity.ERROR
+              case els => s.Message.Severity.UNKNOWN
+            }
+            s.Message(Some(range), severity, info.msg)
           }
-        }
-        val cwd = new java.io.File("").getAbsolutePath.split("/")
-        if (start.startsWith(cwd)) start = start.drop(cwd.length)
-        val msg = s"Compiling $count files in ${start.mkString("/")}"
-        val spaces = " " * ((90 - msg.length) / 2)
-        global.reporter.echo(
-          scala.Console.BLUE_B +
-            spaces + msg + spaces +
-            scala.Console.RESET
-        )
+        case els =>
+          global.reporter.warning(NoPosition, s"Unknown reporter $els")
+          Nil
       }
-      def name: String = phaseName
+    override def newPhase(prev: Phase) = new StdPhase(prev) {
       def apply(unit: global.CompilationUnit): Unit = {
-        
+        val messages = getMessages(unit.source)
+        val source = Paths.get(unit.source.file.file.getAbsoluteFile.toURI)
+        val filename = config.relativePath(source)
+        val attributes = s.Attributes(
+          filename = filename.toString,
+          contents = unit.source.content.mkString,
+          dialect = "Scala210", // TODO
+          names = Nil,
+          messages = messages.toSeq,
+          denotations = Nil,
+          sugars = Nil
+        )
+        val outFile =
+          config.semanticdbPath(filename)
+        global.reporter.info(unit.targetPos,
+                             s"""Persisting attributes for ${unit.source} to $outFile
+                                |$attributes""".stripMargin,
+                             true)
+        val semanticdbOutFile = config.semanticdbPath(filename)
+        semanticdbOutFile.toFile.getParentFile.mkdirs()
+        global.reporter.info(NoPosition, semanticdbOutFile.toFile.exists().toString, true)
+        Files.write(semanticdbOutFile, attributes.toByteArray)
         global.reporter.info(unit.targetPos, s"UNIT: $unit", true)
       }
     }
